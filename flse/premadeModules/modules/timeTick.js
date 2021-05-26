00533f5afef2339488c66691234cec7ecd5693e1
Ejaz Ali <ejazali4@protonmail.ch>
2021-05-26 23:34:55 +0100

    🐛 Fixed error

--- flse/premadeModules/modules/timeTick.js
+++ flse/premadeModules/modules/timeTick.js
@@ -9,7 +9,7 @@ setInterval(() => {
     const hour = timeHandler.getHours().toString().padStart(2, '0');
     const minute = timeHandler.getMinutes().toString().padStart(2, '0');
     const second = timeHandler.getSeconds().toString().padStart(2, '0');
-    document.getElementById(random).innerHTML = `${hour}:${minute}.${second}`
+    try { document.getElementById(random).innerHTML = `${hour}:${minute}.${second}`; } catch(error) {}
 }, 100);
 
 return `<div id="${random}">${hour}:${minute}.${second}</div>`;
