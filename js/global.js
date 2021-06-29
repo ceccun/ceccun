7f84382c79f4efe6b47c25ece9e3472f15a974d9
Ejaz Ali <ejazali4@protonmail.ch>
2021-06-29 18:31:51 +0100

    💄 Fixed yucky align

--- js/global.js
+++ js/global.js
@@ -4,3 +4,30 @@ var date = new Date();
 flsestrings["copyright"] = {
     "default": `© Fivnex, LLC ${date.getUTCFullYear().toString()} `,
 }
+flsestrings["nointernet"] = {
+    "default": "No Internet Connection.",
+    "ja": "インターネット接続はありません。",
+    "es": "Sin conexión a Internet",
+    "fr": "Pas de connexion Internet.",
+    "it": "Nessuna connessione internet."
+}
+
+const checkNetwork = () => {
+    const networkRequest = new XMLHttpRequest();
+    networkRequest.open("GET","https://api.ceccun.com/cdynamic/captive", async=false);
+    networkRequest.onreadystatechange = function (e) {
+        if ((this.status == 200) && (this.readyState == 4)) {
+            try{
+            response = JSON.parse(this.responseText);
+            if (response["error"] == "1"){
+                if ("content" in response) {
+                    return true;
+                }
+            }
+            }
+            catch (error) {
+                return false;
+            }
+        }
+    }
+}
--- js/screens/login.js
+++ js/screens/login.js
@@ -39,6 +39,7 @@ let redirect = new URL(window.location).searchParams.get("redirect");
 const loginScrLogin = () => {
     const email = document.getElementById("email-input").value;
     const password = document.getElementById("password-input").value;
+    console.log(checkNetwork());
     document.getElementById("login-btn").setAttribute("style", "background-color: var(--button-unavailable); transition: none;");
     fetch("https://api.ceccun.com/api/v1/login", {
         "method": "POST",
